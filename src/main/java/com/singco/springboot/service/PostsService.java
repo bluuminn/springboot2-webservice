b9ffea38c856b9434dea689199c15b2b31a0fd97
bluumin <bluuminn@gmail.com>
2019-12-22 19:39:02 +0900

    전체 조회 화면 만들기

--- src/main/java/com/singco/springboot/service/PostsService.java
+++ src/main/java/com/singco/springboot/service/PostsService.java
@@ -2,12 +2,17 @@ package com.singco.springboot.service;
 
 import com.singco.springboot.domain.posts.Posts;
 import com.singco.springboot.domain.posts.PostsRepository;
+import com.singco.springboot.web.dto.PostsListResponseDto;
 import com.singco.springboot.web.dto.PostsResponseDto;
 import com.singco.springboot.web.dto.PostsSaveRequestDto;
 import com.singco.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -34,5 +39,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
