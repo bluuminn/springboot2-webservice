148d99bfe64e7ed49deb2101d1ad5454412d0170
bluumin <bluuminn@gmail.com>
2019-12-26 17:37:08 +0900

    어노테이션 기반으로 개선하기

--- src/main/java/com/singco/springboot/web/IndexController.java
+++ src/main/java/com/singco/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.singco.springboot.web;
 
+import com.singco.springboot.config.auth.LoginUser;
 import com.singco.springboot.config.auth.dto.SessionUser;
 import com.singco.springboot.service.PostsService;
 import com.singco.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
